6aead55014bad35761937565ae300fb6fe6c8cf4
AnasMisbahuddin <anas.misbahuddin4869@gmail.com>
2019-08-28 04:56:20 +0800

    add router vue and display meetups and cateogories

---
+++ src/pages/PageMeetupDetail.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>mettup detail</h1>
+</template>
+
+<script>
+export default {
+    created(){
+        const meetupId = this.$route.params.id
+    }
+}
+</script>
+
+<style>
+
+</style>
